f90c72bd58dbfa73e17122a85fec496109cd9299
tulanilinca <tulan.ilinca@accenture.com>
2024-08-19 16:53:50 +0300

    -added tables for Products,ProductTypes,BOMs,UOMs,Containers

--- TorqueAndTread/TorqueAndTread.Server/Context/TorqueDbContext.cs
+++ TorqueAndTread/TorqueAndTread.Server/Context/TorqueDbContext.cs
@@ -16,6 +16,16 @@ namespace TorqueAndTread.Server.Context
         public DbSet<UserRole> UserRoles { get; set; }
         public DbSet<MenuItem> MenuItems { get; set; }
         public DbSet<MenuItemRole> MenuItemRoles { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductBOM> ProductBOMs { get; set; }
+
+        public DbSet<BOM> BOMs { get; set; }
+
+        public DbSet<UOM> UOMs { get; set; }
+
+        public DbSet<Container> Containers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().HasMany(e=>e.Roles).WithMany(e=>e.Users)
@@ -51,10 +61,76 @@ namespace TorqueAndTread.Server.Context
             modelBuilder.Entity<MenuItemActionRole>().HasOne(e => e.CreatedBy).WithMany().OnDelete(DeleteBehavior.NoAction);
             modelBuilder.Entity<MenuItemActionRole>().HasOne(e => e.LastUpdatedBy).WithMany().OnDelete(DeleteBehavior.NoAction);
 
+            modelBuilder.Entity<Product>().HasOne(p => p.ProductType).WithOne(pt => pt.Product).HasForeignKey<Product>(p => p.ProductTypeId);
+
+            modelBuilder.Entity<Product>().Property(p => p.ProductCodeName).IsRequired();
+
+            modelBuilder.Entity<Product>().Property(p => p.Active).IsRequired();
+
+            modelBuilder.Entity<Product>().Property(p => p.CreatedBy).IsRequired();
+
+            modelBuilder.Entity<Product>().Property(p => p.CreatedOn).IsRequired();
+
+            modelBuilder.Entity<Product>().Property(p => p.LastUpdatedBy).IsRequired(false);
+
+            modelBuilder.Entity<Product>().Property(p => p.LastUpdatedOn).IsRequired(false);
+
+            //Required fields for ProductType
+
+            modelBuilder.Entity<ProductType>().Property(pt => pt.ProductTypeName).IsRequired();
+
+            modelBuilder.Entity<ProductType>().Property(pt => pt.Active).IsRequired();
+
+            modelBuilder.Entity<ProductType>().Property(pt => pt.CreatedBy).IsRequired();
+
+            modelBuilder.Entity<ProductType>().Property(pt => pt.CreatedOn).IsRequired();
+
+            modelBuilder.Entity<ProductType>().Property(pt => pt.LastUpdatedBy).IsRequired(false);
+
+            modelBuilder.Entity<ProductType>().Property(pt => pt.LastUpdatedOn).IsRequired(false);
+
+            //Required fields for ProductBOM
+
+            modelBuilder.Entity<Product>().HasMany(p => p.ProductBOMs).WithOne(pb => pb.Product).HasForeignKey(pb => pb.ProductId);
+
+            modelBuilder.Entity<ProductBOM>().Property(pb => pb.Active).IsRequired();
+
+            modelBuilder.Entity<ProductBOM>().Property(pb => pb.CreatedBy).IsRequired();
+
+            modelBuilder.Entity<ProductBOM>().Property(pb => pb.CreatedOn).IsRequired();
+
+            modelBuilder.Entity<ProductBOM>().Property(pb => pb.LastUpdatedBy).IsRequired(false);
+
+            modelBuilder.Entity<ProductBOM>().Property(pb => pb.LastUpdatedOn).IsRequired(false);
+
+            //Required fields for BOM
+
+            modelBuilder.Entity<BOM>().HasMany(b => b.ProductBOM).WithOne(pb => pb.BOM).HasForeignKey(pb => pb.BOMId);
+
+            modelBuilder.Entity<BOM>().Property(pb => pb.Active).IsRequired();
+
+            modelBuilder.Entity<BOM>().Property(pb => pb.CreatedBy).IsRequired();
+
+            modelBuilder.Entity<BOM>().Property(pb => pb.CreatedOn).IsRequired();
+
+            modelBuilder.Entity<BOM>().Property(pb => pb.LastUpdatedBy).IsRequired(false);
+
+            modelBuilder.Entity<BOM>().Property(pb => pb.LastUpdatedOn).IsRequired(false);
+
+            //Required fields for Container
+
+            modelBuilder.Entity<BOM>().HasMany(b => b.Containers).WithOne(c => c.BOM).HasForeignKey(c => c.BOMId);
+
+            //Required fields for UOM
+
+            modelBuilder.Entity<UOM>().HasOne(u => u.Container).WithOne(c => c.UOM).HasForeignKey<Container>(c => c.UOMId);
+
+
+
 
             //seeding
 
-            var userSeed=LoadUserSeedDataFromFile();
+            var userSeed =LoadUserSeedDataFromFile();
             modelBuilder.Entity<User>().HasData(userSeed);
         }
         private List<UserSeedObject> LoadUserSeedDataFromFile()
