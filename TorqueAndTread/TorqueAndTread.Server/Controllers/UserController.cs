c3b194e5214d8dd757a3583bd0ef614629bdb834
accTurcsa <alexandru.a.turcsa@accenture.com>
2024-08-26 13:31:25 +0300

    Add Toaster Popups

--- TorqueAndTread/TorqueAndTread.Server/Controllers/UserController.cs
+++ TorqueAndTread/TorqueAndTread.Server/Controllers/UserController.cs
@@ -5,41 +5,48 @@ using TorqueAndTread.Server.Services;
 
 namespace TorqueAndTread.Server.Controllers
 {
-  [Route("/api/[controller]")]
-  [ApiController]
-  public class UserController : ControllerBase
-  {
-    private UserService _userService;
-    public UserController(UserService userService)
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
-      _userService = userService;
-    }
+        private UserService _userService;
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
 
-    [HttpGet("all")]
-    public async Task<IActionResult> GetAllUsers()
-    {
-      var users = await _userService.GetAllUsers();
-      return Ok(users);
-    }
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _userService.GetAllUsers();
+            return Ok(users);
+        }
 
-    [HttpGet("{id:int}")]
-    public async Task<IActionResult> GetUser(int id)
-    {
-      var user = await _userService.GetUser(id);
-      return Ok(user);
-    }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _userService.GetUser(id);
+            return Ok(user);
+        }
 
-    [HttpPut]
-    public async Task<IActionResult> UpdateUser([FromBody]UserAbvrDTO userAbvrDTO)
-    {
-      await _userService.EditUser(userAbvrDTO, -1);
-      return Ok(new { Message="success"});
+        [HttpPut]
+        public async Task<IActionResult> UpdateUser([FromBody] UserAbvrDTO userAbvrDTO)
+        {
+            await _userService.EditUser(userAbvrDTO, -1);
+            return Ok(new { Message = "success" });
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddUser([FromBody] UserCreateDTO userAbvrDTO)
+        {
+            await _userService.CreateUser(userAbvrDTO, -1);
+            return Ok(new { Message = "success" });
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            await _userService.SoftDeleteUser(id,-1);
+            return Ok(new { Message = "success" });
+
+        }
     }
-    [HttpPost]
-    public async Task<IActionResult> AddUser([FromBody] UserCreateDTO userAbvrDTO)
-    {
-      await _userService.CreateUser(userAbvrDTO, -1);
-      return Ok(new { Message = "success" });
-    }
-  }
 }
